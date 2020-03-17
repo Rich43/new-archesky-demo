04c88014144b77884b1c41fccb8799d9eb2281d5
Richard Ward <RichieS@GMail.com>
2020-03-17 19:47:03 +0000

    Add skeleton app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ApolloProvider from 'react-apollo/ApolloProvider';
+import {ApolloProvider as ApolloHooksProvider} from '@apollo/react-hooks';
+import {client} from "./graphql";
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from "./components/Home";
+
+export const HOME = '/';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ApolloProvider client={client}>
+        <ApolloHooksProvider client={client}>
+          <BrowserRouter>
+              <Route path={HOME} component={Home} />
+          </BrowserRouter>
+        </ApolloHooksProvider>
+      </ApolloProvider>
   );
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Home() {
+    return <>Hello world</>;
+}
+
+export default Home;
