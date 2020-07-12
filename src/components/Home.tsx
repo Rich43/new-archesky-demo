2448ee7b4bb049b6700d20c046fd192079da1ad3
Richie Ward <RichieS@GMail.com>
2020-07-12 13:11:41 +0100

    Send first request, need to sort out Authentication header

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
-import {Box} from "@material-ui/core";
+import React, { useState } from 'react';
+import { Box, Button, TextField } from "@material-ui/core";
+import { useCreateContentMutation } from "../graphql/createContent";
 
 export function Home() {
-    return <Box p={2}>Hello world</Box>;
+    const [content, setContent] = useState('');
+    const [createContent] = useCreateContentMutation();
+    const onSubmit = () => {
+        console.log(content);
+        createContent(
+            {
+                variables: {content}
+            }
+        ).then();
+    };
+    return (
+        <>
+            <Box p={2}>{ content }</Box>
+            <TextField
+                value={content}
+                onChange={event => setContent(event.target.value)}
+            />
+            <Button onClick={() => onSubmit()}>Submit</Button>
+        </>
+    );
 }
 
 export default Home;
