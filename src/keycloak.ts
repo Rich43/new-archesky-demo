5db4a58339f020a2933741cbe8611c75f58589b7
Richie Ward <RichieS@GMail.com>
2020-08-25 22:03:11 +0100

    realm name is lowercase now for some reason

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -6,7 +6,7 @@ import Keycloak from 'keycloak-js';
 // @ts-ignore
 const keycloak = new Keycloak({
     url: '/auth/',
-    realm: 'Archesky_localhost', // TODO: make this generic
+    realm: 'archesky_localhost', // TODO: make this generic
     clientId: 'archesky'
 });
 
