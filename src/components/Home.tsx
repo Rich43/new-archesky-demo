657a3e33b61004e7b08da95992479129c98a07b9
Richard Ward <richies@gmail.com>
2022-01-07 20:02:01 +0000

    Output list of pages

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
 import { Box, Button, TextField } from "@material-ui/core";
+import useSWR from 'swr';
 
 export function Home() {
     const [content, setContent] = useState('');
+    const fetcher = (url: string) => fetch(url).then(r => r.json());
+    const { data, error } = useSWR('/content-api/content', fetcher);
+
     const onSubmit = () => {
         console.log(content);
     };
     return (
         <>
+            <p>Error: { JSON.stringify(error, null, 4) }</p>
+            <pre>
+                { JSON.stringify(data, null, 4) }
+            </pre>
             <Box p={2}>{ content }</Box>
             <TextField
                 value={content}
