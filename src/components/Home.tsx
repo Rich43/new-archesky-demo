d169520c8139060f00904f59a31ed421505212e4
Richard Ward <richies@gmail.com>
2022-01-08 19:13:32 +0000

    Code cleanup

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,36 +1,49 @@
-import React, { useState } from 'react';
-import { Box, Button, TextField } from "@material-ui/core";
-import useSWR, { useSWRConfig } from 'swr';
+import React, {useState} from 'react';
+import {Box, Button, TextField} from "@material-ui/core";
+import useSWR, {useSWRConfig} from 'swr';
+import {useKeycloak} from "@react-keycloak/web";
 
 const CONTENT_API_URL = '/content-api/content';
+
+const headers = (token: string) => ({
+    'content-type': 'application/json;charset=UTF-8',
+    hostname: window.location.hostname,
+    Authorization: `Bearer ${token}`
+});
 
 export function Home() {
     const [content, setContent] = useState('');
     const fetcher = (url: string) => fetch(url).then(r => r.json());
-    const { data, error } = useSWR(CONTENT_API_URL, fetcher);
-    const { mutate } = useSWRConfig();
+    const {data, error} = useSWR(CONTENT_API_URL, fetcher);
+    const {mutate} = useSWRConfig();
+    const [keycloak, initialized] = useKeycloak();
 
-    const onSubmit = () => {
-        console.log(content);
-        mutate(CONTENT_API_URL, {
+    const onSubmit = async () => {
+        const extraData = {
             "createdBy": "Richard",
             "displayName": content,
             "name": content,
             "published": true
-        }, false).then();
+        };
+        await fetch(CONTENT_API_URL, {
+            method: 'POST',
+            headers: (initialized && headers(keycloak.token)) || {},
+            body: JSON.stringify(extraData)
+        });
+        mutate(CONTENT_API_URL).then();
     };
     return (
         <>
-            <p>Error: { JSON.stringify(error, null, 4) }</p>
+            <p>Error: {JSON.stringify(error, null, 4)}</p>
             <pre>
-                { JSON.stringify(data, null, 4) }
+                {JSON.stringify(data, null, 4)}
             </pre>
-            <Box p={2}>{ content }</Box>
+            <Box p={2}>{content}</Box>
             <TextField
                 value={content}
                 onChange={event => setContent(event.target.value)}
             />
-            <Button onClick={() => onSubmit()}>Submit</Button>
+            <Button onClick={() => onSubmit()} disabled={!initialized}>Submit</Button>
         </>
     );
 }
